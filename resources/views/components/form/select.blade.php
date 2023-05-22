4aef27a99126c63051968dd82fb3d6f224932208
Dhayu Intan Nareswari <intaandh@gmail.com>
2023-05-22 11:40:06 +0700

    Select

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}</label>
-    <select class="custom-select" id="{{ $name }}" {{ $attributes }}>
+    <select class="custom-select" id="{{ $name }}" name="{{ $name }}" {{ $attributes }}>
         <option value="">Pilih {{ $label }}</option>
         @foreach ($initValues as $item)
         <option value="{{ $item }}" {{ $item===$value ? 'selected' : '' }}>{{ $item }}</option>
